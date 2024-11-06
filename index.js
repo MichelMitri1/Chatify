d2cfbb510ca83b42959abbdded7d3910e6b5d940
MichelMitri1 <michelmitri5@gmail.com>
2024-11-06 12:01:44 +0200

    inshallah

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const PORT = 3000;
 const userRoutes = require("./src/routes/userRoutes");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
